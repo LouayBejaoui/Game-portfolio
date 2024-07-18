c78f6fc54474f8b4ec9e2eb39767fe21dede8df4
Louay Bejaoui <47712690+LouayBejaoui@users.noreply.github.com>
2024-07-18 17:29:21 +0200

    main commit

---
+++ game-portfolio/src/const.js
@@ -0,0 +1,14 @@
+export const conScale = 3 ;
+
+
+export const dialogueData = {
+  skills: `I manipulate HTML&CSS, Javascript/typescript, React,js, Bootstrap, Jquery, Python, Cloud, Git, SQL and NoSQL databases, Web design and security`,
+    me: `I am a creative and passionate programmer with a background in security and a strong
+    track record of bringing ideas to life through innovative design and coding. I thrive on
+    transforming concepts into practical solutions.`,
+    Education: `I completed high school in 2018 with a focus on computer science. Subsequently, I earned my bachelor's degree in Network Security from the Higher Institute of Technological Studies in Communications of Tunis. I then obtained my master's degree in computer engineering from Vistula University in Warsaw Poland.`,
+    projects: `You can check out my projects on github: "https://www.github.com/LouayBejaoui"`,
+    library: `yomken houni skills azber`,
+    ontactMe: `You can contact me vis Linkedin: "www.linkedin.com/in/louay-bejaoui" 
+    Or through my email: louay.bejaoui@edu.isetcom.tn.`,
+  };
